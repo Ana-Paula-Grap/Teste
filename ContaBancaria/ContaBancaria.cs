06f58b4f03e65f423dc7fe650c3651f19878c0e3
Ana Paula Dos Santos De Sá <ana.dos.santos.de.sa@avanade.com>
2024-01-16 18:33:39 -0300

    projetos teste

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    public class ContaBancariaC
+    {
+        private readonly int numeroConta;
+        private string titularConta;
+        private double saldo;
+
+        public ContaBancariaC(int numeroConta, string titularConta)
+        {
+            this.numeroConta = numeroConta;
+            this.titularConta = titularConta;
+            this.saldo = 0.0;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            if (valorDeposito > 0)
+            {
+                saldo += valorDeposito;
+                Console.WriteLine($"Depósito de ${valorDeposito} realizado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("O valor do depósito deve ser maior que zero.");
+            }
+
+            MostrarDadosConta();
+        }
+
+        public void Sacar(double valorSaque)
+        {
+            double taxaSaque = 3.50;
+
+            if (saldo >= valorSaque + taxaSaque)
+            {
+                saldo -= (valorSaque + taxaSaque);
+                Console.WriteLine($"Saque de ${valorSaque} realizado com sucesso. Taxa de ${taxaSaque} aplicada.");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+            }
+
+            MostrarDadosConta();
+        }
+
+        public void MostrarDadosConta()
+        {
+            Console.WriteLine($"Dados da conta:\nConta {numeroConta}, Titular: {titularConta}, Saldo: ${saldo:F2}\n");
+        }
+
+        public void AlterarTitular(string novoTitular)
+        {
+            titularConta = novoTitular;
+            Console.WriteLine($"Titular da conta alterado para: {novoTitular}");
+        }
+    }
+}
+
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,55 @@
+﻿using ContaBancaria;
+using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Exemplo 1
+        ContaBancariaC conta1 = new ContaBancariaC(5447, "Milton Gonçalves");
+        conta1.MostrarDadosConta();
+
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        char resposta1 = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (resposta1 == 's' || resposta1 == 'S')
+        {
+            Console.Write("Entre o valor de depósito inicial: ");
+            double depositoInicial1 = Convert.ToDouble(Console.ReadLine());
+            conta1.Depositar(depositoInicial1);
+        }
+
+        Console.Write("Entre um valor para depósito: ");
+        double deposito1 = Convert.ToDouble(Console.ReadLine());
+        conta1.Depositar(deposito1);
+
+        Console.Write("Entre um valor para saque: ");
+        double saque1 = Convert.ToDouble(Console.ReadLine());
+        conta1.Sacar(saque1);
+
+        // Exemplo 2
+        ContaBancariaC conta2 = new ContaBancariaC(5139, "Elza Soares");
+        conta2.MostrarDadosConta();
+
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        char resposta2 = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (resposta2 == 's' || resposta2 == 'S')
+        {
+            Console.Write("Entre o valor de depósito inicial: ");
+            double depositoInicial2 = Convert.ToDouble(Console.ReadLine());
+            conta2.Depositar(depositoInicial2);
+        }
+
+        Console.Write("Entre um valor para depósito: ");
+        double deposito2 = Convert.ToDouble(Console.ReadLine());
+        conta2.Depositar(deposito2);
+
+        Console.Write("Entre um valor para saque: ");
+        double saque2 = Convert.ToDouble(Console.ReadLine());
+        conta2.Sacar(saque2);
+    }
+}
